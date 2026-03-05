09a7e296cffc205517ec27fec075dd14c5a988ef
jerim <jerimielid7@gmail.com>
2026-03-05 12:46:52 +0300

    Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
 fun main(){
-  //Code Body
-  println("Welcome to Desktop")
+  //Variables
+  //camelCase = 'c'
+
+  var userName: String = "Jeremy"
+  println("Hello $userName!")
+
+  //Interpreter reads line by line, from up to down
+  userName= "Blessed"
+  println("Hello $userName!")
+
+  //Koltin is statically typed, it checks variables at compile time not runtime
+
+  // val - value can't be changed after value is assigned to it. it's like static
+  val name: String = "Jeremy"
+  println("Hello $name!")
+
+  // Koltin can auto detect value types, it infers the type by the first type that is assigned to it. Meaning the first type locks it
+  var age: Int = 0
+  var ages = 10
+
+  println("$age years old")
+
+  //Use shift+f10 to run code
 }
