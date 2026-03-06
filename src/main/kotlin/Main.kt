c209e20681fce942eab73521f79674844cb7a818
jerim <jerimielid7@gmail.com>
2026-03-06 11:31:50 +0300

    Operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,29 @@
 fun main(){
-  val name: String = "Michelle"
-  val age: Int = 18
+  //Crtl + K - Commit
+  //Crtl + Shift + K - Push
+  //Crtl + Alt + N - New Branch
 
-  //Reason for the grayed out string is that it is a redundant type.
+  val x = 1
+  val y = 2F
 
-  println("Hello, $name. I hear you're age is $age")
-  println("Are you sure you're supposed to be here.")
+  val result = x + y
 
-  var max = Integer.MAX_VALUE //Maximum value that can be stored in an int
-  var min = Integer.MIN_VALUE //Minmum value that can be stored in an int
+  println(result)
 
-  println(max)
-  println(min)
+  val result2 = x % y // Gives the remainder of division
+  println(result2)
 
+  //Operator BODMAS
+  var result3 = 3+4/5*3
+  println("3+4/5*3 = $result3")
 
-  //Byte
-  val byteMaxValue: Byte = Byte.MAX_VALUE
-  val byteMinValue: Byte = Byte.MIN_VALUE
+  result3+=2
+  println(result3)
 
-  println("Byte Values")
-  println(byteMaxValue)
-  println(byteMinValue)
-
-
-  //Short
-  val shortMaxValue: Short = Short.MAX_VALUE
-  val shortMinValue: Short = Short.MIN_VALUE
-
-  println("Short Values")
-  println(shortMaxValue)
-  println(shortMinValue)
-
-  //Whole Numbers
-  val longMaxValue: Long = Long.MAX_VALUE
-  val longMinValue: Long = Long.MIN_VALUE
-
-  println("Long Values")
-  println(longMaxValue)
-  println(longMinValue)
-
-  //If no type is specified kotlin will assign it into any category that fits it, Int, Long, Short. But default type is int
-  val myNumber = 2800000000
-
-  //Ways of declaring long types
-  val myNumber3 = 28000000000
-  val myNumber2 = 12L
-
+  println(result3++)
+  println(++result3)
 
 
 
 }
+
