46c040744eacd56e563b982b94a3dbf056fe85ad
jerim <jerimielid7@gmail.com>
2026-03-09 08:07:14 +0300

    Operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
   //Crtl + K - Commit
   //Crtl + Shift + K - Push
   //Crtl + Alt + N - New Branch
@@ -14,21 +14,32 @@ fun main(){
   println(result2)
 
   //Operator BODMAS
-  var result3 = 3+4/5*3
+  var result3 = 3 + 4 / 5 * 3
   println("3+4/5*3 = $result3")
 
-  result3+=2
+  result3 += 2
   println(result3)
 
   println(result3++)
   println(++result3)
 
-  val isActive = true
-    //Control flow statements
-    if(isActive){
-      println("I'm active")
-    }
+  val isActive = false
+  //Control flow statements
+  if (isActive) {
+    println("I'm active")
+  } else {
+    println("I'm inactive")
+  }
+
+  val mynum = 12
+  if (mynum >= 4){
+    println("It is")
+  }else{
+    println("No")
+  }
 
 
 }
+
+
 
