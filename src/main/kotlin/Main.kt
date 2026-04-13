52579ec507c38afe904538bd1247b13707269fe7
Jeremy <jerimielid7@gmail.com>
2026-04-13 11:35:36 +0300

    Operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,9 @@
-fun main() {
-  //Crtl + K - Commit
-  //Crtl + Shift + K - Push
-  //Crtl + Alt + N - New Branch
-
-  val x = 1
-  val y = 2F
-
-  val result = x + y
-
-  println(result)
-
-  val result2 = x % y // Gives the remainder of division
-  println(result2)
-
-  //Operator BODMAS
-  var result3 = 3 + 4 / 5 * 3
-  println("3+4/5*3 = $result3")
-
-  result3 += 2
-  println(result3)
-
-  println(result3++)
-  println(++result3)
-
-  val isActive = false
-  //Control flow statements
-  if (isActive) //Simplified Boolean Expression
-  {
-    println("I'm active")
-  } else {
-    println("I'm inactive")
-  }
-
-  val mynum = 12
-  if (mynum >= 4){
-    println("It is")
-  }
-  else if (mynum == 12){
-    println("12")
-  }
-  else{
-    println("No")
-  }
-
-
-  val score = 80
-
-  if(isActive && score > 90){
-    println("Keep going")
+fun main(args: Array<String> ) {
+  val text: String? = null
+  if (text != null) {
+    println(text)
+    println(text.length)
   }else{
-    println("Try again")
+    println("Variable is null")
   }
-
 }
-
-
-
